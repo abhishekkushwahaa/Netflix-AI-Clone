3e73feef5956c8301bcf3bf2e4e5deb498a68aef
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-01-21 09:51:09 +0530

    add movie genres

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,60 +1,81 @@
-import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress, Icon} from "@mui/material"
-import { Link } from "react-router-dom"
-import useStyles from './Styles'
-import { Home } from "@mui/icons-material"
-import HdOutlinedIcon from '@mui/icons-material/HdOutlined';
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  ListItemIcon,
+  Box,
+  CircularProgress,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import useStyles from "./Styles";
+import { Home } from "@mui/icons-material";
+import HdOutlinedIcon from "@mui/icons-material/HdOutlined";
+import { useGetGenresQuery } from "../../services/tmdb";
 
- 
-const categories = [ 
-    {label: 'Popular', value: 'popular'},
-    {label: 'Top Rated', value: 'top-rated'},
-    {label: 'Upcoming', value: 'upcoming'},
-]
+const categories = [
+  { label: "Popular", value: "popular" },
+  { label: "Top Rated", value: "top-rated" },
+  { label: "Upcoming", value: "upcoming" },
+];
 
 const movieCategories = [
-    {label: 'Comedy', value: 'comedy'},
-    {label: 'Horror', value: 'horror'},
-    {label: 'Action', value: 'action'},
-    {label: 'Animation', value: 'animation'},
-]
+  { label: "Comedy", value: "comedy" },
+  { label: "Horror", value: "horror" },
+  { label: "Action", value: "action" },
+  { label: "Animation", value: "animation" },
+];
 
-const Sidebar = ({setMobileOpen}) => { 
+const Sidebar = ({ setMobileOpen }) => {
   const Classes = useStyles();
+  const { data, isFetching } = useGetGenresQuery();
+  // console.log(data);
   return (
     <>
-      <Link to='/' className={Classes.imageLink}>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg' alt='Netflix Logo' className={Classes.image}/>
+      <Link to="/" className={Classes.imageLink}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          alt="Netflix Logo"
+          className={Classes.image}
+        />
       </Link>
       <Divider />
       <List>
         <ListSubheader>Categories</ListSubheader>
-        {categories.map(({label, value}) => (
-          <Link key={value} className={Classes.links} to='/'>
+        {categories.map(({ label, value }) => (
+          <Link key={value} className={Classes.links} to="/">
             <ListItem onClick={() => {}} button>
               <ListItemIcon>
-              <Home/>
+                <Home />
               </ListItemIcon>
-            <ListItemText primary={label} />
+              <ListItemText primary={label} />
             </ListItem>
           </Link>
         ))}
       </List>
-        <Divider/>
+      <Divider />
       <List>
-      <ListSubheader>Genres</ListSubheader>
-      {movieCategories.map(({label, value}) => (
-        <Link key={value} className={Classes.links} to='/'>
-          <ListItem onClick={() => {}} button>
-            <ListItemIcon>
-            <HdOutlinedIcon/>
-            </ListItemIcon>
-          <ListItemText primary={label} />
-          </ListItem>
-        </Link>
-      ))}
-      </List> 
+        <ListSubheader>Genres</ListSubheader>
+        {isFetching ? (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress />
+          </Box>
+        ) : (
+          data.genres.map(({ name, id }) => (
+            <Link key={name} className={Classes.links} to="/">
+              <ListItem onClick={() => {}} button>
+                <ListItemIcon>
+                  <HdOutlinedIcon />
+                </ListItemIcon>
+                <ListItemText primary={name} />
+              </ListItem>
+            </Link>
+          ))
+        )}
+      </List>
     </>
-  )
-} 
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -9,10 +9,16 @@ export const tmdbApi = createApi({
     baseUrl: "https://api.themoviedb.org/3",
   }),
   endpoints: (builder) => ({
+    // Get Genres
+    getGenres: builder.query({
+      query: () => `/genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
+
+    // Get Popular Movies
     getPopularMovies: builder.query({
       query: () => `/movie/popular?page=${page}&api_key=${tmdbApiKey}`,
     }),
   }),
 });
 
-export const { useGetPopularMoviesQuery } = tmdbApi;
+export const { useGetPopularMoviesQuery, useGetGenresQuery } = tmdbApi;
