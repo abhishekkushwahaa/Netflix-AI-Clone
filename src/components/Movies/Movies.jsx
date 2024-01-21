c3611945fb0b5a92442653bf689fc346c8288396
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-01-21 09:36:38 +0530

    movie view

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,22 +1,47 @@
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, Grow, Rating, Tooltip } from "@mui/material";
 import useStyle from "./Styles";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const Movies = ({ movie }) => {
+const Movies = ({ movie, i }) => {
+  const textColor = "black";
   // console.log("movie title:", movie.title); // Log the movie title
   const classes = useStyle();
 
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movies}>
-      <Typography className={classes.title} variant="h5">
-        {movie.title}
-      </Typography>
+      <Grow in key={i} timeout={(i + 1) * 250}>
+        <Link className={classes.links} to={`/movies/${movie.id}`}>
+          <img
+            alt={movie.title}
+            className={classes.image}
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                : "https://www.fillmurray.com/200/300"
+            }
+          />
+          <Typography
+            className={classes.title}
+            variant="h6"
+            style={{ color: textColor }}
+          >
+            {movie.title}
+          </Typography>
+          <Tooltip disableTouchListener title={`${movie.vote_average} / 10`}>
+            <div>
+              <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+            </div>
+          </Tooltip>
+        </Link>
+      </Grow>
     </Grid>
   );
 };
 
 Movies.propTypes = {
   movie: PropTypes.object.isRequired,
+  i: PropTypes.number.isRequired,
 };
 
 export default Movies;
--- src/components/Movies/Styles.jsx
+++ src/components/Movies/Styles.jsx
@@ -1,17 +1,42 @@
 import { makeStyles } from "@mui/styles";
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   movies: {
     padding: "10px",
+    overflow: "hidden",
+  },
+  links: {
+    alignItems: "center",
+    fontFamily: "Roboto",
+    fontWeight: "regular",
+    textDecoration: "none",
+    [theme.breakpoints.down("xs")]: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    "&:hover": {
+      cursor: "pointer",
+      textDecoration: "none",
+    },
+  },
+  image: {
+    marginTop: "10px",
+    justifyContent: "space-between",
+    width: "100%",
+    height: "300px",
+    borderRadius: "10px",
+    transition: "transform 0.2s ease-in-out",
+    "&:hover": {
+      transform: "scale(1.05)",
+    },
   },
   title: {
     color: "theme.palette.text.primary",
     textOverflow: "ellipsis",
     width: "230px",
-    overflow: "hidden",
-    whiteSpace: "nowrap",
-    marginTop: "10px",
+    whiteSpace: "wrap",
+    marginTop: "5px",
     marginBottom: 0,
-    textAlign: "center",
+    textAlign: "left",
   },
 }));
